786c9e20427426b411140838b4dfedf2b80f4beb
Manish Kumar Rai <manishkumarrai3389@gmail.com>
2025-07-04 17:48:36 +0200

    Variables and Functions

--- ch02.scala
+++ ch02.scala
@@ -1,1 +1,20 @@
 //  First code in Scala
+
+// ----------------- REPL Code
+
+// println("Hello, World!")
+
+// variables
+
+//val msg = "Manish"
+
+// val msg2: java.lang.String = "Rai"
+
+//val msg3: String = "Hello yet again, world!"
+
+// Functions
+
+def max(x:Int,y:Int) = {
+    if x > y then x
+    else y
+}
