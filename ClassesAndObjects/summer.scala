db44883685bec0d119b4e3dc45b57727c75c9360
Manish Kumar Rai <manishkumarrai3389@gmail.com>
2025-07-07 09:02:40 +0200

    Classes And Objects

--- ClassesAndObjects/summer.scala
+++ ClassesAndObjects/summer.scala
@@ -1,11 +1,17 @@
 import ChecksumAccumulator.calculate
 
-object Summer {
-  def main(args: Array[String]): Unit = {
-    for (arg <- args) {
-      println(s"$arg: ${calculate(arg)}")
+// object Summer {
+//   def main(args: Array[String]): Unit = {
+//     for (arg <- args) {
+//       println(s"$arg: ${calculate(arg)}")
+//     }
+//   }
+// }
+
+@main def Summer(args:String*) = {
+    for arg <- args do {
+        println(s"$arg: ${calculate(arg)}")
     }
-  }
 }
 
 
