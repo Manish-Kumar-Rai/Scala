9796f67da55d3390bef0e694cbeb259884102d8c
Manish Kumar Rai <manishkumarrai3389@gmail.com>
2025-07-07 04:24:59 +0200

    Second Step Completed

--- SecondStep/ch03.scala
+++ SecondStep/ch03.scala
@@ -58,10 +58,102 @@
 // val hashSet = HashSet("Tomatoes", "Chilies")
 // val ingredients = hashSet + "Coriande
 
-// Map
+// Map and HashMap
 // import scala.collection.mutable.Map
 
 // val capital = Map("USA"->"Washington","France"->"Paris")
 // capital += ("India"->"Delhi")
 
-// HashMap
+// Learn to recognize the functional style
+//  Note: var for imperative and val for functional
+// Imperative Style using var
+// def printArgs(args:List[String]):Unit = {
+//     var i = 0
+//     while i < args.length do
+//         println(args(i))
+//         i += 1
+// }
+
+// You can transform this bit of code into a more functional style by getting rid of the var
+// def printArgs(args:List[String]):Unit =
+//     for arg <- args do
+//         println(arg)
+
+//  --- Or this
+// def printArgs(args:List[String]):Unit = {
+//     args.foreach(println)
+// }
+
+// A more functional approach would be to define a method that formats
+// the passed args for printing, but just returns the formatted string
+
+// def formatStrings(args:List[String]) = args.mkString("\n")
+
+// Transform with map and for-yield
+
+/*
+When programming in an imperative style, you mutate data structures in
+place until you achieve the goal of the algorithm. In a functional style, you
+transform immutable data structures into new ones to achieve the goal
+*/
+
+// An important method that facilitates functional transformations on immutable collections is map
+// val adjectives = List("One","Two","Red","Blue")
+
+// val nouns = adjectives.map(adj => adj + " Fish.")
+
+/*
+Another way to perform this transformation is to use a for expression in
+which you introduce the body with the keyword yield instead of do
+*/
+// val nouns = {
+//     for adj <- adjectives yield
+//         adj + " Fish"
+// }
+/*
+Note: The for-yield produces exactly the same result as map because the compiler transforms
+ the for-yield expression into the map call.
+*/
+
+/*
+Note: If the function passed to map results in a different type, then
+the List returned by map will have that type as its element type
+*/
+
+// val lengths = nouns.map(noun => noun.length)
+// val lengths = {
+//     for noun <- nouns yield
+//         noun.length
+// }
+
+/*
+The map method appears on many types, not just List. This enables
+for expressions to be used with many types. One example is Vector, which
+is an immutable sequence that provides “effectively constant time” performance for all its operations.
+*/
+
+// val ques = Vector("Who","Where","What","Why")
+
+// val usingMap = ques.map(q => q.toLowerCase() + "?")
+
+// val usingForYield = {
+//     for q <- ques yield
+//         q.toLowerCase() + "?"
+// }
+
+/*
+OPTIONS:
+    An Option is either a Some, which indicates that a
+value exists, or None, which indicates that no value exists
+*/
+
+// the result type of find is always Option[String]
+// val startsW = ques.find(q=>q.startsWith("W")) //Some(Who)
+// val hasLen4 = ques.find(q=> q.length == 4)   //Some(What)
+// val hasLen5 = ques.find(q=> q.length == 5)  //Some(Where)
+// val startsH = ques.find(q=> q.startsWith("H"))  //None
+
+/*
+One way to think of Option, however, is as a collection that contains either zero (the
+None case) or one (the Some case) elements.
+*/
