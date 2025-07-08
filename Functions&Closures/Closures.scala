710b89e19dbe2812ae70b07654a554bb78908b7f
Manish Kumar Rai <manishkumarrai3389@gmail.com>
2025-07-08 20:52:40 +0200

    Closures continue

--- Functions&Closures/Closures.scala
+++ Functions&Closures/Closures.scala
@@ -1,1 +1,11 @@
 // -----------------Closures
+/*
+In Scala, a closure is a function that captures the bindings of free variables (variables defined outside the function) and retains access to them even when the function is used in a different context.
+*/
+
+val more = 10
+val addMore = (x:Int) => x + more
+
+/*
+In Scala, "surrounding scope" refers to the block of code (or environment) outside a function or expression where variables are defined and available.
+*/
